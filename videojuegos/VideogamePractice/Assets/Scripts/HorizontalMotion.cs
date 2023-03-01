3ec44bf1eb7004b36abfb343758b970cea0d199d
Bebo Rico <beborico16@hotmail.com>
2023-03-01 11:53:04 -0600

    Added HorizontalMotion

--- videojuegos/VideogamePractice/Assets/Scripts/HorizontalMotion.cs
+++ videojuegos/VideogamePractice/Assets/Scripts/HorizontalMotion.cs
@@ -9,7 +9,8 @@ using UnityEngine;
 
 public class HorizontalMotion : MonoBehaviour {
     [SerializeField] float speed;
-
+    [SerializeField] float limit;
+    
     Vector3 move;
 
     void Start() { // Start is called before the first frame update
@@ -18,6 +19,13 @@ public class HorizontalMotion : MonoBehaviour {
 
     void Update() { // Update is called once per frame
         move.x = Input.GetAxis("Horizontal");
+
+        //Limit the motion to a specific range of coordinates
+        if (transform.position.x < limit && move.x < 0) {
+            move.x = 0;
+        } else if  (transform.position.x > limit && move.x > 0) {
+            move.x = 0;
+        }
 
         Debug.Log("X motion: " + move.x); // Esto es como un print
 
