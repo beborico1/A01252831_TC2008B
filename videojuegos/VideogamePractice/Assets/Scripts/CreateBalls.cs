7dbfcf6a0ee548e7c6f75f9e4164d6ca82985af6
Bebo Rico <beborico16@hotmail.com>
2023-03-01 11:39:19 -0600

    Created HorizontalMotion

--- videojuegos/VideogamePractice/Assets/Scripts/CreateBalls.cs
+++ videojuegos/VideogamePractice/Assets/Scripts/CreateBalls.cs
@@ -6,17 +6,18 @@ public class CreateBalls : MonoBehaviour
 {
     // Class variable to be changed from the Unity interface
     [SerializeField] GameObject ball; // Serialize Field hace que se pueda ver desde la interfaz de Unity
+    [SerializeField] float delay; // Serialize Field hace que se pueda ver desde la interfaz de Unity
 
     // Start is called before the first frame update
-    void Start()
-    {
-        // Call the "DropBall" function after 0.5 seconds
-        // and then keep calling it every 0.8 seconds
-        InvokeRepeating("DropBall()", 0.5f, 0.8f);
+    void Start() {
+        InvokeRepeating("DropBall", 0.5f, delay); // Call the "DropBall" function after 0.5 seconds and then keep calling it every 0.8 seconds
     }
 
-    void DropBall() 
-    {
-        Instantiate(ball);
+    void DropBall() {
+        Vector3 pos = new Vector3(Random.Range(-10.0f, 10.0f), 6, 0); // Generate a new random position
+
+        GameObject obj = Instantiate(ball, pos, Quaternion.identity); // Create a copy of the prefab
+
+        Destroy(obj, 10); // Doom the object do die in 5 seconds
     }
 }
---
+++ videojuegos/VideogamePractice/Assets/Scripts/HorizontalMotion.cs
@@ -0,0 +1,22 @@
+// Move a GameObject using the arrow keys or AD
+
+// Luis Carlos Rico Almada
+// 2023-03-1
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMotion : MonoBehaviour {
+    Vector3 move;
+
+    void Start() { // Start is called before the first frame update
+        
+    }
+
+    void Update() { // Update is called once per frame
+        move.x = Input.GetAxis("Horizontal");
+
+        Debug.Log("X motion: " + move.x); // Esto es como un print
+    }
+}
